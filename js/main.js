046295af6920ab0b70a703454abc82515a410c96
lszyszka <l.szyszka90@gmail.com>
2019-01-18 15:37:28 +0100

    Mobile&Desktop

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ const footerWrap = document.querySelector('footer .wrap');
 const footer = document.querySelector('footer');
 
 //showFooterAnimationOnScroll
+var lastScrollPosition = 0;
 window.addEventListener('scroll', function () {
     let scrollPosition = this.scrollY;
     let footerFromTop = footer.offsetTop;
     if (scrollPosition > footerFromTop - window.innerHeight) {
         footerWrap.classList.add('active');
     }
-
 });
