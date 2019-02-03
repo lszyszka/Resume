0f07d4fb850ca6efe40c101295a24fcaecbc7691
lszyszka <l.szyszka90@gmail.com>
2019-02-03 17:35:21 +0100

    add color and animaition

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const footerWrap = document.querySelector('footer .wrap');
 const footer = document.querySelector('footer');
 
+const projectDiv = document.querySelectorAll('.one-project');
+
 //showFooterAnimationOnScroll
 var lastScrollPosition = 0;
 window.addEventListener('scroll', function () {
@@ -10,3 +12,20 @@ window.addEventListener('scroll', function () {
         footerWrap.classList.add('active');
     }
 });
+
+//show Projects Section Animation On Scroll
+window.addEventListener('scroll', function () {
+    let scrollPosition = this.scrollY;
+    let firstDivProject = projectDiv[0].offsetTop;
+    if (scrollPosition > firstDivProject - window.innerHeight) {
+        projectDiv[0].classList.add('active');
+    }
+    let secondDivProject = projectDiv[1].offsetTop;
+    if (scrollPosition > secondDivProject - window.innerHeight) {
+        projectDiv[1].classList.add('active');
+    }
+    let thirdDivProject = projectDiv[2].offsetTop;
+    if (scrollPosition > thirdDivProject - window.innerHeight) {
+        projectDiv[2].classList.add('active');
+    }
+})
